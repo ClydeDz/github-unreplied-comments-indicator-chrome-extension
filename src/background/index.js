5d8cf76f8f5d6292afad26dbeb5adcf9e189d411
Clyde D'Souza <clyd94@gmail.com>
2025-08-16 18:13:53 +1200

    Safely handle ext reloading error

--- src/background/index.js
+++ src/background/index.js
@@ -1,30 +1,40 @@
 import { CHROME_SYNC_STORAGE_THEME_KEY, getStorage } from "../common/storage";
 import { THEMES } from "../common/themes";
 
-setInterval(async function () {
-  const selectedTheme = await getStorage(CHROME_SYNC_STORAGE_THEME_KEY);
+const intervalId = setInterval(async function () {
+  try {
+    const selectedTheme = await getStorage(CHROME_SYNC_STORAGE_THEME_KEY);
 
-  Array.from(document.querySelectorAll(".js-comments-holder"))
-    .filter((parent) => {
-      const commentDivs = parent.querySelectorAll(".js-comment");
-      return commentDivs.length === 1;
-    })
-    .forEach((parent) => {
-      const themeClassnames = ["github-unreplied-comments"];
-      parent.classList.remove(
-        "github-unreplied-comments-leftBorder-theme",
-        "github-unreplied-comments-pulsating-theme",
-        "github-unreplied-comments-backgroundcolor-theme"
-      );
+    Array.from(document.querySelectorAll(".js-comments-holder"))
+      .filter((parent) => {
+        const commentDivs = parent.querySelectorAll(".js-comment");
+        return commentDivs.length === 1;
+      })
+      .forEach((parent) => {
+        const themeClassnames = ["github-unreplied-comments"];
+        parent.classList.remove(
+          "github-unreplied-comments-leftBorder-theme",
+          "github-unreplied-comments-pulsating-theme",
+          "github-unreplied-comments-backgroundcolor-theme"
+        );
 
-      if (selectedTheme === THEMES.leftBorder) {
-        themeClassnames.push("github-unreplied-comments-leftBorder-theme");
-      } else if (selectedTheme === THEMES.pulsating) {
-        themeClassnames.push("github-unreplied-comments-pulsating-theme");
-      } else if (selectedTheme === THEMES.backgroundColor) {
-        themeClassnames.push("github-unreplied-comments-backgroundcolor-theme");
-      }
+        if (selectedTheme === THEMES.leftBorder) {
+          themeClassnames.push("github-unreplied-comments-leftBorder-theme");
+        } else if (selectedTheme === THEMES.pulsating) {
+          themeClassnames.push("github-unreplied-comments-pulsating-theme");
+        } else if (selectedTheme === THEMES.backgroundColor) {
+          themeClassnames.push(
+            "github-unreplied-comments-backgroundcolor-theme"
+          );
+        }
 
-      parent.classList.add(...themeClassnames);
-    });
+        parent.classList.add(...themeClassnames);
+      });
+  } catch {
+    clearInterval(intervalId);
+    console.info(
+      "⚠️ GitHub Unreplied Comments Indicator:",
+      "Please reload the page since the extension was reloaded"
+    );
+  }
 }, 2000);
