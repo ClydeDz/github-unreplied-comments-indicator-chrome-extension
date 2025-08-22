9ec327e3cda09020867ed2e27d56dfad65a348a8
Clyde D'Souza <clyd94@gmail.com>
2025-08-22 18:51:41 +1200

    Remove highlight from replied comments

--- src/background/index.js
+++ src/background/index.js
@@ -1,35 +1,46 @@
 import { CHROME_SYNC_STORAGE_THEME_KEY, getStorage } from "../common/storage";
-import { THEMES } from "../common/themes";
+import { CLASSNAMES, THEMES } from "../common/themes";
+
+const removeClassnames = (parent) => {
+  parent.classList.remove(
+    CLASSNAMES[THEMES.leftBorder],
+    CLASSNAMES[THEMES.pulsating],
+    CLASSNAMES[THEMES.backgroundColor]
+  );
+};
+
+const removeHighlightFromRepliedComments = () => {
+  Array.from(document.querySelectorAll(".js-comments-holder"))
+    .filter((parent) => {
+      const commentDivs = parent.querySelectorAll(".js-comment");
+      return commentDivs.length > 1;
+    })
+    .forEach((parent) => {
+      removeClassnames(parent);
+    });
+};
+
+const addHighlightToUnrepliedComments = (selectedTheme) => {
+  Array.from(document.querySelectorAll(".js-comments-holder"))
+    .filter((parent) => {
+      const commentDivs = parent.querySelectorAll(".js-comment");
+      return commentDivs.length === 1;
+    })
+    .forEach((parent) => {
+      removeClassnames(parent);
+
+      const themeClassnames = ["github-unreplied-comments"];
+      themeClassnames.push(CLASSNAMES[selectedTheme]);
+
+      parent.classList.add(...themeClassnames);
+    });
+};
 
 const intervalId = setInterval(async function () {
   try {
     const selectedTheme = await getStorage(CHROME_SYNC_STORAGE_THEME_KEY);
-
-    Array.from(document.querySelectorAll(".js-comments-holder"))
-      .filter((parent) => {
-        const commentDivs = parent.querySelectorAll(".js-comment");
-        return commentDivs.length === 1;
-      })
-      .forEach((parent) => {
-        const themeClassnames = ["github-unreplied-comments"];
-        parent.classList.remove(
-          "github-unreplied-comments-leftBorder-theme",
-          "github-unreplied-comments-pulsating-theme",
-          "github-unreplied-comments-backgroundcolor-theme"
-        );
-
-        if (selectedTheme === THEMES.leftBorder) {
-          themeClassnames.push("github-unreplied-comments-leftBorder-theme");
-        } else if (selectedTheme === THEMES.pulsating) {
-          themeClassnames.push("github-unreplied-comments-pulsating-theme");
-        } else if (selectedTheme === THEMES.backgroundColor) {
-          themeClassnames.push(
-            "github-unreplied-comments-backgroundcolor-theme"
-          );
-        }
-
-        parent.classList.add(...themeClassnames);
-      });
+    removeHighlightFromRepliedComments();
+    addHighlightToUnrepliedComments(selectedTheme);
   } catch {
     clearInterval(intervalId);
     console.info(
--- src/common/themes.js
+++ src/common/themes.js
@@ -3,3 +3,9 @@ export const THEMES = {
   leftBorder: "leftBorder",
   pulsating: "pulsating",
 };
+
+export const CLASSNAMES = {
+  [THEMES.backgroundColor]: "github-unreplied-comments-backgroundcolor-theme",
+  [THEMES.leftBorder]: "github-unreplied-comments-leftBorder-theme",
+  [THEMES.pulsating]: "github-unreplied-comments-pulsating-theme",
+};
